422bfba912fe2a9a93e7549eb7b191b35e4e126b
lucaslingle <lucasdaxlingle@gmail.com>
2021-05-18 22:37:17 -0700

    commit refactor

--- play.py
+++ play.py
@@ -9,7 +9,7 @@ def play(env, agent, comm, args):
         o_t = env.reset()
         while t < args.max_timesteps:
             _ = env.render()
-            pi_dist, vpred, _ = agent(tc.Tensor(o_t).float().unsqueeze(0))
+            pi_dist, vpred, _ = agent(tc.tensor(o_t).float().unsqueeze(0))
             a_t = pi_dist.sample()
             o_tp1, r_t, done_t, _ = env.step(a_t.squeeze(0).detach().numpy())
             total_reward += r_t
